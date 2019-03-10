8f5e5e029c2f801a2977fb76e200efc87a6cb01c
yang-chris <18813068824@163.com>
2019-03-10 23:22:12 +0800

    视频轮播

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(function(){
-  var swiper = new Swiper('.swiper-container', {
+  var swiper = new Swiper('.swiper-banner', {
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
@@ -26,5 +26,28 @@ $(function(){
     let curIndex = $(this).index();
     $(this).addClass(className).siblings().removeClass(className);
     $tabContent.eq(curIndex).addClass(className).siblings().removeClass(className);
+  });
+
+
+  // 视频切换
+  var videoSwiper = $('#videoSwiper');
+  var videoWrapper = $('#videoWrapper');
+  var slideSize  = videoSwiper.find('.video-slide').size();
+  var slideWidth = videoWrapper.outerWidth(true);
+  videoSwiper.find('.video-slide').width(slideWidth)
+  videoWrapper.width(slideWidth * slideSize)
+  $('#videoTitle').on('click', 'a', function(){
+    console.log($(this).attr('class'))
+    let curClassName = $(this).attr('class');
+    if(curClassName === 'btn-next'){
+      console.log(11)
+      videoSwiper.animate({
+        left: -slideWidth
+      }, 500);
+    }else if(curClassName === 'btn-prev'){
+
+    }
+    // if($(this).className())
   })
+  console.log(videoWrapper.outerWidth(true), slideSize);
 })
